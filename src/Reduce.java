87bed72edfecc70c0ac654fc98b1bd46eeff6027
Yodishtr Vythilingum <yodishtrvythilingum@Yodishtrs-MacBook-Pro.local>
2024-09-13 01:29:44 -0400

    new file Reduce

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+
+    public static void main(String[] args) {
+
+        int steps = 0;
+        int n = 100;
+        while (n > 0) {
+            if ((n % 2) == 0) {
+                n = n / 2;
+                steps ++;
+            } else if ((n % 2) != 0) {
+                n = n - 1;
+                steps ++;
+            }
+        }
+        System.out.println(steps);
+    }
+}
